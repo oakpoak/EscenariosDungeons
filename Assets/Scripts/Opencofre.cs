4c9f1f07c08a754e184639bf0a95acec923b1000
Moonn03 <samantha_sanchez@anahuacmayab.edu.mx>
2024-12-05 18:02:01 -0600

    Cofreee
    No funciona abrir

---
+++ Assets/Scripts/Opencofre.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opencofre : MonoBehaviour
+{
+    public Animator animator;  // El Animator de la cofreabriendo
+    private bool isOpened = false;  // Si la cofre  ya fue abierta
+
+    void Start()
+    {
+        // Obtén el componente Animator de la cofre
+        animator = GetComponent<Animator>();
+    }
+
+    // Sobrescribimos el método Interact para definir qué sucede cuando el jugador interactúa con la puerta
+    public  void Interact()
+    {
+        if (!isOpened)
+        {
+            isOpened = true;
+            animator.SetBool("Abrir", true);  // Activamos el parámetro "Abrir" en el Animator
+        }
+        else
+        {
+            Debug.Log("El cofre ya está abierta.");
+        }
+    }
+}
