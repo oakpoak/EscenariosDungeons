3095bb3e680bc50c58449a871863e24a5e3dcb36
Tony2399 <163464729+Tony2399@users.noreply.github.com>
2025-04-29 18:37:28 -0600

    PlayerINTOSCENE

---
+++ Assets/Scripts/PracticumLevel/FollowAndSwitchCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowAndSwitchCamera : MonoBehaviour
+{
+    
+    [Header("Referencias")]
+    public Transform target;          // Tu personaje
+
+    [Header("Offset inicial (se calcula al Start)")]
+    private Vector3 offset;           // distancia fija y altura fija
+
+    private void Start()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("No hay target asignado en FollowAndSwitchCamera");
+            return;
+        }
+        // Calculamos el desplazamiento inicial entre cámara y jugador
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        // Sólo actualizamos la posición: target + offset
+        transform.position = target.position + offset;
+    }
+}
+
+
+
